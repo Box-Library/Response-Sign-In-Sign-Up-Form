8b89c162d87007831f8a700605b2a00e7ca3f0eb
Theeban <69495468+Theebanraj-Uthayakumar@users.noreply.github.com>
2021-11-09 07:18:20 +0530

    V 1.0.3

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 
-const Routing = () => {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
-  );
-};
+// const Routing = () => {
+//   return (
+//     <Switch>
+//       <Route path="/" exact component={Home} />
+//     </Switch>
+//   );
+// };
 
 function App() {
   return (
     <>
-      <Router>
+      {/* <Router>
         <Routing />
-      </Router>
+      </Router> */}
+      <h1>Theebanraj Uthayakumar</h1>
     </>
   );
 }
