5bd989ff40c7c4057e48357cf62befbcb9171875
Theeban <69495468+Theebanraj-Uthayakumar@users.noreply.github.com>
2021-11-09 07:23:41 +0530

    V 1.0.4

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 
-// const Routing = () => {
-//   return (
-//     <Switch>
-//       <Route path="/" exact component={Home} />
-//     </Switch>
-//   );
-// };
+const Routing = () => {
+  return (
+    <Switch>
+      <Route path="" exact component={Home} />
+    </Switch>
+  );
+};
 
 function App() {
   return (
     <>
-      {/* <Router>
+      <Router>
         <Routing />
-      </Router> */}
-      <h1>Theebanraj Uthayakumar</h1>
+      </Router>
+      {/* <h1>Theebanraj Uthayakumar</h1> */}
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Home() {
-  return <div>Hi there this is home page</div>;
+  return (
+    <div>
+      <center>
+        <h1>Hi there this is home page</h1>
+        <h2>Theebanraj Uthayakumar</h2>
+      </center>
+    </div>
+  );
 }
 
 export default Home;
