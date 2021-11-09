e10668e3bb19241d36de22c8d0a08281c17b0b66
Theeban <69495468+Theebanraj-Uthayakumar@users.noreply.github.com>
2021-11-09 21:41:06 +0530

    V 1.1.2

--- src/App.js
+++ src/App.js
@@ -9,20 +9,11 @@ import {
 import Home from "./components/Home";
 import Temperature from "./components/Temperature";
 
-// const Routing = () => {
-//   return (
-//     <Switch>
-//       <Route exact path="/" component={Home} />
-//       <Route exact path="/Temperature" component={Temperature} />
-//     </Switch>
-//   );
-// };
-
 function App() {
   return (
     <>
       <Router>
-        {/* <Routing /> */}
+        {/* <Header/> */}
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/Temperature" exact component={Temperature} />
